39977977246c9edb2ac80167a47dff5b63ec873d
FathullaLatipov <fathullaxonlatipov@gmail.com>
2026-02-08 01:09:01 +0500

    3

--- config/settings.py
+++ config/settings.py
@@ -39,7 +39,12 @@ TEMPLATES = [
     },
 ]
 
-DATABASES = {}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 LANGUAGE_CODE = "ru-ru"
 TIME_ZONE = "Europe/Moscow"
